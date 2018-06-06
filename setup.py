22013658a12bf9fc2f665c8265c5a46babbc6bf4
Anne Maiale <anne@stitchdata.com>
2018-06-06 14:30:12 -0400

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-listrak",
-    version="0.1.0",
+    version="1.0.0",
     description="Singer.io tap for extracting data from the Jira API",
     author="Stitch",
     url="http://singer.io",
