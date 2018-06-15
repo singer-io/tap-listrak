0f83a943d35481ee635dce5cbb3a99fd4f95537d
Dan Mosora <dan@stitchdata.com>
2018-06-15 17:22:06 -0400

    Pin singer-python at the last tested version (5.0.3) and patch version bump

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name="tap-listrak",
-    version="1.0.0",
+    version="1.0.1",
     description="Singer.io tap for extracting data from the Jira API",
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_listrak"],
     install_requires=[
-        "singer-python>=3.2.0",
+        "singer-python==5.0.3",
         "requests",
         "zeep",
     ],
