f84302cec99ae66010e82fc5c29613c556943df6
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-20 16:28:39 +0530

    Addressed review comments

--- tap_listrak/http.py
+++ tap_listrak/http.py
@@ -1,12 +1,10 @@
 import zeep
-import sys
 import singer
 from singer import metrics
 from zeep.exceptions import Fault, TransportError, XMLSyntaxError
 import backoff
 
 LOGGER = singer.get_logger()
-
 WSDL = "https://webservices.listrak.com/v31/IntegrationService.asmx?wsdl"
 
 def get_client(config):
@@ -18,12 +16,14 @@ def get_client(config):
     return client
 
 def log_retry_attempt(details):
-    """Log details about a backoff retry attempt."""
-    _, exception, _ = sys.exc_info()
-    LOGGER.info("Retryable exception: %s", exception)
-    LOGGER.info("Retry attempt %s. Waiting %s seconds before next try...",
-                details["tries"], details["wait"])
+    """Log retry attempt with clean message."""
+    exception = details["exception"]
+    LOGGER.warning(
+        "Retrying due to error (attempt %s): %s. Waiting %s seconds before next try...",
+        details["tries"], str(exception), details["wait"]
+    )
 
+# Robust retry handler for common SOAP failure types
 @backoff.on_exception(
     backoff.expo,
     (XMLSyntaxError, TransportError, Fault),
@@ -32,7 +32,7 @@ def log_retry_attempt(details):
     on_backoff=log_retry_attempt
 )
 def request(tap_stream_id, service_fn, **kwargs):
-    """Make SOAP API request with error handling and retry."""
+    """Make SOAP API request with retry and structured error logging."""
     with metrics.http_request_timer(tap_stream_id) as timer:
         try:
             response = service_fn(**kwargs)
@@ -41,6 +41,18 @@ def request(tap_stream_id, service_fn, **kwargs):
                         tap_stream_id, kwargs.get('Page'), kwargs.get('StartDate'))
             return response
 
+        except XMLSyntaxError as e:
+            LOGGER.exception("Malformed XML in stream '%s': %s", tap_stream_id, str(e))
+            raise
+
+        except Fault as e:
+            LOGGER.exception("SOAP Fault in stream '%s': %s", tap_stream_id, str(e))
+            raise
+
+        except TransportError as e:
+            LOGGER.exception("Transport error in stream '%s': %s", tap_stream_id, str(e))
+            raise
+
         except Exception as e:
-            LOGGER.exception("Unhandled exception in stream '%s' : %s", tap_stream_id, str(e))
+            LOGGER.exception("Unexpected exception in stream '%s': %s", tap_stream_id, str(e))
             raise
