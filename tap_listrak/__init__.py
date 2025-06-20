06e1a891e2130643507d31ce5027e4080defb2a2
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-20 16:45:00 +0530

    SAC-27860: updated setup.py and __init__.py to support discovery mode with selected metadata

--- tap_listrak/__init__.py
+++ tap_listrak/__init__.py
@@ -11,49 +11,53 @@ from . import schemas
 REQUIRED_CONFIG_KEYS = ["start_date", "username", "password"]
 LOGGER = singer.get_logger()
 
-
 def check_credentials_are_authorized(ctx):
-    pass
-
+    pass  # Temporarily disabled due to no credentials
 
 def discover(ctx):
-    check_credentials_are_authorized(ctx)
+    # Skip credential check
+    # check_credentials_are_authorized(ctx)
+
     catalog = Catalog([])
 
     for tap_stream_id in schemas.stream_ids:
         schema_dict = schemas.load_schema(tap_stream_id)
         schema = Schema.from_dict(schema_dict)
 
-        mdata = metadata.get_standard_metadata(schema_dict,
-                                               key_properties=schemas.PK_FIELDS[tap_stream_id])
-
+        mdata = metadata.get_standard_metadata(
+            schema_dict,
+            key_properties=schemas.PK_FIELDS[tap_stream_id])
+        
         mdata = metadata.to_map(mdata)
 
-        # NB: `lists` and `messages` are required for their substreams.
-        # This is an approximation of the initial functionality using
-        # metadata, which marked them as `selected=True` in the schema.
+        # Auto-include critical parent streams
         if tap_stream_id in ['lists', 'messages']:
             mdata = metadata.write(mdata, (), 'inclusion', 'automatic')
 
+        # Auto-mark each field as "selected": true (if not unsupported)
         for field_name in schema_dict['properties'].keys():
             mdata = metadata.write(mdata, ('properties', field_name), 'inclusion', 'automatic')
+
+        md_list = metadata.to_list(mdata)
+        for md in md_list:
+            if md["metadata"].get("inclusion") != "unsupported":
+                md["metadata"]["selected"] = True
 
         catalog.streams.append(CatalogEntry(
             stream=tap_stream_id,
             tap_stream_id=tap_stream_id,
             key_properties=schemas.PK_FIELDS[tap_stream_id],
             schema=schema,
-            metadata = metadata.to_list(mdata)
+            metadata=md_list
         ))
-    return catalog
 
+    return catalog
 
 def sync(ctx):
     for tap_stream_id in ctx.selected_stream_ids:
         schemas.load_and_write_schema(tap_stream_id)
     streams_.sync_lists(ctx)
     ctx.write_state()
-
 
 def main_impl():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
@@ -65,7 +69,6 @@ def main_impl():
         ctx.catalog = Catalog.from_dict(args.properties) \
             if args.properties else discover(ctx)
         sync(ctx)
-
 
 def main():
     try:
