45cfdf52ea99107c3d34160d3ceecc076d6e1c67
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-25 15:21:28 +0530

    Updated http.py for unit test passing

--- tap_listrak/http.py
+++ tap_listrak/http.py
@@ -1,8 +1,7 @@
 import zeep
-import sys
 import singer
 from singer import metrics
-from zeep.exceptions import Fault
+from zeep.exceptions import Fault, TransportError, XMLSyntaxError
 import backoff
 
 LOGGER = singer.get_logger()
@@ -10,6 +9,7 @@ LOGGER = singer.get_logger()
 WSDL = "https://webservices.listrak.com/v31/IntegrationService.asmx?wsdl"
 
 def get_client(config):
+    """Initialize the SOAP client with authentication headers."""
     client = zeep.Client(wsdl=WSDL)
     elem = client.get_element("{http://webservices.listrak.com/v31/}WSUser")
     headers = elem(UserName=config["username"], Password=config["password"])
@@ -17,23 +17,31 @@ def get_client(config):
     return client
 
 def log_retry_attempt(details):
-    _, exception, _ = sys.exc_info()
-    LOGGER.info(exception)
-    LOGGER.info('Caught retryable error after %s tries. Message: %s. Waiting %s more seconds then retrying...',
-                details["tries"],
-                exception.message,
-                details["wait"])
+    """Log details about a backoff retry attempt."""
+    exception = details.get("exception")
+    LOGGER.warning(
+        "Retry attempt %s due to error: %s. Waiting %s more seconds before retrying...",
+        details["tries"],
+        str(exception),
+        details["wait"]
+    )
 
+@backoff.on_exception(
+    backoff.expo,
+    (XMLSyntaxError, TransportError, Fault),
+    max_tries=5,
+    jitter=None,
+    on_backoff=log_retry_attempt
+)
 def request(tap_stream_id, service_fn, **kwargs):
+    """Make SOAP API request with retry, metrics, and centralized error logging."""
     with metrics.http_request_timer(tap_stream_id) as timer:
-        try:
-            response = service_fn(**kwargs)
-            timer.tags[metrics.Tag.http_status_code] = 200
-            LOGGER.info("Making request for message %s page %s with start date: %s",
-                        kwargs.get('MsgID'), kwargs.get('Page'), kwargs.get('StartDate'))
-            return response
-        except Fault as e:
-            if "404" in str(e.detail):
-                LOGGER.info("Encountered a 404 for message: %s", kwargs['MsgID'])
-                return None
-            raise
+        response = service_fn(**kwargs)
+        timer.tags[metrics.Tag.http_status_code] = 200
+        LOGGER.info(
+            "Request successful for stream: %s | Page: %s | Start: %s",
+            tap_stream_id,
+            kwargs.get('Page', 'N/A'),
+            kwargs.get('StartDate', 'N/A')
+        )
+        return response
