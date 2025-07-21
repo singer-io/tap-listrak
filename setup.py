e44f2c840c37001e570422e70b3a6497084233c8
Ben Allred <ben.allred@qlik.com>
2025-07-21 12:03:15 -0600

    SAC-27925: bump dependencies for compliance (#22)
    * Update setup.py
    
    * Bump tap version and update changelog (#23)

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 
 setup(
     name="tap-listrak",
-    version="1.1.0",
+    version="1.1.1",
     description="Singer.io tap for extracting data from the Listrak API",
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_listrak"],
     install_requires=[
-        "singer-python==5.8.1",
-        "requests==2.20.0",
+        "singer-python==5.13.2",
+        "requests==2.32.4",
         "zeep",
-        'backoff==1.8.0',
+        'backoff==1.10.0',
         'pendulum==1.2.0',
     ],
     entry_points="""
