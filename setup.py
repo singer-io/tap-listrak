819ea518596e56d1a780e6fc8db3092e98e62144
Ranit Saha <ranit.saha@globallogic.com>
2026-02-18 09:59:15 +0530

    Updated dependencies

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     py_modules=["tap_listrak"],
     install_requires=[
         "singer-python==6.7.0",
-        "requests==2.32.4",
-        "zeep",
+        "requests==2.32.5",
+        "zeep==4.3.2",
         'backoff==2.2.1',
         'pendulum==3.1.0'
     ],
